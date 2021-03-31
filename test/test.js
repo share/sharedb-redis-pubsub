ad89e8994b86d720da15ad5dab1cfcb9b163ff9a
Alec Gibson <12036746+alecgibson@users.noreply.github.com>
2021-03-31 17:08:49 +0100

    👷 Add linting
    This change adds linting config read across from [`sharedb`][1].
    
    [1]: https://github.com/share/sharedb/blob/48267579f37d79b69d3f53426d3eb007ef1f48d0/.eslintrc.js

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
-var RedisPubSub = require('../index');
+var redisPubSub = require('../index');
 
 require('sharedb/test/pubsub')(function(callback) {
-  callback(null, RedisPubSub());
+  callback(null, redisPubSub());
 });
