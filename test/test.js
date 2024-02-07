6708c9f0a79a64576f2fe81d97801196bc6eaeef
Alec Gibson <12036746+alecgibson@users.noreply.github.com>
2024-02-07 16:16:24 +0000

    💥 Bump `redis` to v4
    Fixes https://github.com/share/sharedb-redis-pubsub/issues/19
    
    This is a **BREAKING** change that upgrades the underlying `redis`
    client to v4, which had so many [breaking changes][1], that it's
    infeasible to maintain backwards-compatibility with v3. If consumers
    need to use v3, they are advised to pin their version of
    `sharedb-redis-pubsub` to v4.
    
    This change adapts to the breakages:
    
     - wrap the returned `Promise`s in callbacks
     - actively connect the `client`, which no longer auto-connects
     - move the `message` event handler into the subscription callback
     - adapt to the new call signature of `client.eval()`
    
    [1]: https://github.com/redis/node-redis/blob/HEAD/docs/v3-to-v4.md

--- test/test.js
+++ test/test.js
@@ -1,5 +1,44 @@
 var redisPubSub = require('../index');
+var redis = require('redis');
+var runTestSuite = require('sharedb/test/pubsub');
 
-require('sharedb/test/pubsub')(function(callback) {
-  callback(null, redisPubSub());
+describe('default options', function() {
+  runTestSuite(function(callback) {
+    callback(null, redisPubSub());
+  });
+});
+
+describe('unconnected client', function() {
+  runTestSuite(function(callback) {
+    callback(null, redisPubSub({
+      client: redis.createClient()
+    }));
+  });
+});
+
+describe('connected client', function() {
+  var client;
+
+  beforeEach(function(done) {
+    client = redis.createClient();
+    client.connect().then(function() {
+      done();
+    }, done);
+  });
+
+  runTestSuite(function(callback) {
+    callback(null, redisPubSub({
+      client: client
+    }));
+  });
+});
+
+describe('connecting client', function() {
+  runTestSuite(function(callback) {
+    var client = redis.createClient();
+    client.connect();
+    callback(null, redisPubSub({
+      client: client
+    }));
+  });
 });
