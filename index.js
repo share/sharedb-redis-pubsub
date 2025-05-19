7f1b1ed92e079c239e2adca5652f66599b732369
Dawid Kisielwski <91346346+dawidreedsy@users.noreply.github.com>
2025-05-19 15:50:33 +0200

    💥 Bump redis version to 5.0.0
    Redis released [version 5](https://github.com/redis/node-redis/releases/tag/redis%405.0.0) , but the only breaking change that is affecting us is:
    
    ```
    Connection Management:
    
    client.QUIT/quit() is replaced by client.close()
    client.disconnect() has been renamed to client.destroy()
    ```
    
    I tried to make it a non breakable change, by allowing both close and quit

--- index.js
+++ index.js
@@ -43,8 +43,8 @@ RedisPubSub.prototype.close = function(callback) {
 
 RedisPubSub.prototype._close = function() {
   return this._closing = this._closing || this._connect().then(Promise.all([
-    this.client.quit(),
-    this.observer.quit()
+    close(this.client),
+    close(this.observer)
   ]));
 };
 
@@ -92,3 +92,11 @@ var PUBLISH_SCRIPT =
   'for i = 2, #ARGV do ' +
     'redis.call("publish", ARGV[i], ARGV[1]) ' +
   'end';
+
+function close(client) {
+  if (client.close) {
+    return client.close();
+  }
+
+  return client.quit();
+}
